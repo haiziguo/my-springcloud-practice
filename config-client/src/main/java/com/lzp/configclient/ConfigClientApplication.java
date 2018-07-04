83b4f17c55a2760b61dd686df98a3605b9d0beda
lizhanpeng <lizhanpeng.juzifenqi.com>
2018-07-04 17:13:18 +0800

    config 分布式

--- config-client/src/main/java/com/lzp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/lzp/configclient/ConfigClientApplication.java
@@ -3,11 +3,14 @@ package com.lzp.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableEurekaClient
+
 public class ConfigClientApplication {
     @Value("${foo}")
     String foo;
--- config-eureka-server/src/main/java/com/lzp/configeurekaserver/ConfigEurekaServerApplication.java
+++ config-eureka-server/src/main/java/com/lzp/configeurekaserver/ConfigEurekaServerApplication.java
@@ -1,16 +1,14 @@
-package com.lzp.configserver;
+package com.lzp.configeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
-@EnableConfigServer
 @EnableEurekaServer
-public class ConfigServerApplication {
+@SpringBootApplication
+public class ConfigEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigEurekaServerApplication.class, args);
 	}
 }
